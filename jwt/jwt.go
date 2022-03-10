c15f18f61ac2a64eb6b5f8f0d0405497f3982aca
jfarroyo291 <j-a-m291@hotmail.com>
2022-03-10 11:42:12 -0500

    modificacion clave

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -10,7 +10,7 @@ import (
 /*GeneroJWT genera el encriptado con JWT*/
 func GeneroJWT(t models.Usuario) (string, error) {
 
-	miClave := []byte("MastersdelDesarrollo_grupodeTwitter")
+	miClave := []byte("MastersDelDesarrollo_grupodeFacebook")
 
 	payload := jwt.MapClaims{
 		"email":            t.Email,
